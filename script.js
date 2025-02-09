e619f764c3fcff810753eadf1e0791e33be6adac
SamDev01 <mureithissam@gmail.com>
2025-02-10 02:24:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+const yesBtn = document.getElementById("yes-btn");
+const definitelyBtn = document.getElementById("definitely-btn");
+const responsePopup = document.getElementById("response-popup");
+const closeBtn = document.getElementById("close-btn");
+
+yesBtn.addEventListener("click", () => {
+    responsePopup.style.display = "flex";
+});
+
+definitelyBtn.addEventListener("click", () => {
+    responsePopup.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", () => {
+    responsePopup.style.display = "none";
+});
